a54dd6154b20f83a98e6c8250a839a2b20062098
Ayas Mahmud <ayasmahmud48@gmail.com>
2025-09-26 08:15:35 +0600

    first commit

---
+++ src/component/navbar/home/Home.jsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+import Navbar from "../Navbar";
+const Home = () => {
+    return (
+        <div>
+        <Navbar></Navbar>
+      <div className=''>
+        <div>
+          <div className='w-[40%] mx-auto '></div>
+              
+          </div>
+      </div> 
+        </div>
+    );
+};
+
+export default Home;
